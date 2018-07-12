dcca1a480060c94793b7d88fa2651ef102a3bbbb
uchida <prex.uchida@gmail.com>
2018-07-13 01:15:34 +0900

    実装修正

--- app/Services/LineService.php
+++ app/Services/LineService.php
@@ -26,14 +26,14 @@ class LineService
 
         sleep(rand(0, 2));
 
-        $messageData = $this->echoBack($json['message']);
-        $this->reply($json['replyToken'], $messageData);
+        $message = $this->getEchoBack($json['message']);
+        $this->push($json['userId'], ['type'=>'text', 'text'=>$message]);
 
         sleep(rand(0, 2));
 
-        $messageData = $this->registerAndInvitation($json['userId']);
-        $this->reply($json['replyToken'], $messageData);
-
+        $email = $this->getTransEmailFromUserId($json['userId']);
+        $message = $this->getInvitation($email);
+        $this->push($json['userId'], ['type'=>'text', 'text'=>$message]);
     }
 
     const EMAIL_REGEXP = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
@@ -59,7 +59,8 @@ class LineService
         return sprintf('line+%s@%s', $token, config('app.email_domain'));
     }
 
-    function registerAndInvitation($user_id){
+    function getTransEmailFromUserId($user_id)
+    {
 
         $obj = $this->emailModel->whereUserId($user_id)->first() ?: (new Email);
 
@@ -68,25 +69,22 @@ class LineService
         $obj->token = md5($user_id);
         $obj->save();
 
-        $transEmail = self::getTransEmail($obj->token);
+        return  self::getTransEmail($obj->token);
+    }
 
-        return [
-            'type' => 'text',
-            'text' => <<<EOD
+    function getInvitation($transEmail){
+
+        return <<<EOD
 $transEmail
 にメールを送るにゃ。
-EOD
-        ];
-
+EOD;
     }
 
-    private function echoBack($text){
+    function getEchoBack($text){
 
-        return [
-            'type' => 'text',
-            'text' => array_random(["{$text}ってなんにゃ？", "{$text}。。知らないにゃ。。", "www", "その話はあとにゃ"])
-        ];
+        return array_random(["{$text}ってなんにゃ？", "{$text}。。知らないにゃ。。", "www", "その話はあとにゃ"]);
     }
+
 
     function receiveEmail($email){
 
@@ -162,108 +160,6 @@ EOD
 
 }
 
-
-/*
-
-// 送られてきたメッセージの中身からレスポンスのタイプを選択
-if ($message->{"text"} == '確認') {
-    // 確認ダイアログタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => '確認ダイアログ',
-        'template' => [
-            'type' => 'confirm',
-            'text' => '元気ですかー？',
-            'actions' => [
-                [
-                    'type' => 'message',
-                    'label' => '元気です',
-                    'text' => '元気です'
-                ],
-                [
-                    'type' => 'message',
-                    'label' => 'まあまあです',
-                    'text' => 'まあまあです'
-                ],
-            ]
-        ]
-    ];
-} elseif ($message->{"text"} == 'ボタン') {
-    // ボタンタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => 'ボタン',
-        'template' => [
-            'type' => 'buttons',
-            'title' => 'タイトルです',
-            'text' => '選択してね',
-            'actions' => [
-                [
-                    'type' => 'postback',
-                    'label' => 'webhookにpost送信',
-                    'data' => 'value'
-                ],
-                [
-                    'type' => 'uri',
-                    'label' => 'googleへ移動',
-                    'uri' => 'https://google.com'
-                ]
-            ]
-        ]
-    ];
-} elseif ($message->{"text"} == 'カルーセル') {
-    // カルーセルタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => 'カルーセル',
-        'template' => [
-            'type' => 'carousel',
-            'columns' => [
-                [
-                    'title' => 'カルーセル1',
-                    'text' => 'カルーセル1です',
-                    'actions' => [
-                        [
-                            'type' => 'postback',
-                            'label' => 'webhookにpost送信',
-                            'data' => 'value'
-                        ],
-                        [
-                            'type' => 'uri',
-                            'label' => '美容の口コミ広場を見る',
-                            'uri' => 'http://clinic.e-kuchikomi.info/'
-                        ]
-                    ]
-                ],
-                [
-                    'title' => 'カルーセル2',
-                    'text' => 'カルーセル2です',
-                    'actions' => [
-                        [
-                            'type' => 'postback',
-                            'label' => 'webhookにpost送信',
-                            'data' => 'value'
-                        ],
-                        [
-                            'type' => 'uri',
-                            'label' => '女美会を見る',
-                            'uri' => 'https://jobikai.com/'
-                        ]
-                    ]
-                ],
-            ]
-        ]
-    ];
-} else {
-    // それ以外は送られてきたテキストをオウム返し
-    $messageData = [
-        'type' => 'text',
-        'text' => $message->{"text"}
-    ];
-}
-
-
-*/
 
 
 
